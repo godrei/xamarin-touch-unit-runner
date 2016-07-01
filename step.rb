e71193e5a472cb064c8b0f5b24dba08bf544a89a
olegoid <olegoid91@gmail.com>
2016-07-01 01:02:18 -0300

    Initial step implementation

--- step.rb
+++ step.rb
@@ -5,6 +5,13 @@ require 'tmpdir'
 require_relative 'xamarin-builder/builder'
 
 # -----------------------
+# --- Constants
+# -----------------------
+
+TOUCH_SERVER_PROJ = "./Touch.Unit/Touch.Server/Touch.Server.csproj"
+TOUCH_SERVER = "./Touch.Unit/Touch.Server/bin/Debug/Touch.Server.exe"
+
+# -----------------------
 # --- Functions
 # -----------------------
 
@@ -19,14 +26,16 @@ end
 #
 # Parse options
 options = {
-  solution: nil,
-  configuration: nil
+  solution: "/Users/olegoid/Projects/xamarin.ios.rocks/Rocks.sln",
+  configuration: "Debug",
+  platform: "iPhoneSimulator"
 }
 
 parser = OptionParser.new do |opts|
   opts.banner = 'Usage: step.rb [options]'
   opts.on('-s', '--solution path', 'Solution') { |s| options[:solution] = s unless s.to_s == '' }
   opts.on('-c', '--configuration config', 'Configuration') { |c| options[:configuration] = c unless c.to_s == '' }
+  opts.on('-l', '--platform platform', 'Platform') { |l| options[:platform] = l unless l.to_s == '' }
   opts.on('-h', '--help', 'Displays Help') do
     exit
   end
@@ -39,6 +48,7 @@ puts
 puts '========== Configs =========='
 puts " * solution: #{options[:solution]}"
 puts " * configuration: #{options[:configuration]}"
+puts " * platform: #{options[:platform]}"
 
 #
 # Validate options
@@ -49,11 +59,39 @@ fail_with_message('No configuration environment found') unless options[:configur
 # Main
 builder = Builder.new(options[:solution], options[:configuration], options[:platform], nil)
 begin
+  # Build Touch.Unit server
+  puts `xbuild #{TOUCH_SERVER_PROJ}`
+
   # The solution has to be built before runing the Touch.Unit tests
   builder.build_solution
 
-  # Executing NUnit tests
-  # TODO
+  output = builder.generated_files
+
+  error_with_message('no output generated') if output.nil? || output.empty?
+  app_file = nil
+
+  output.each do |_, project_output|
+    app_file = project_output[:app] if project_output[:api] == Api::IOS
+  end
+
+  error_with_message('*.app required to run Touch.Unit tests') unless app_file
+
+  # run app on simulator with mono debugger attached
+  `mono --debug #{TOUCH_SERVER} --launchsim #{app_file} -autoexit -logfile=test.log`
+
+  # check test logs
+  if File.exist?('test.log')
+  	File.open('test.log', "r") do |f|
+  	  f.each_line do |line|
+        puts line
+      end
+    end
+  else
+  	error_with_message('Cant find test logs')
+  end
+
+  # show results
+
 rescue => ex
   error_with_message(ex.inspect.to_s)
   error_with_message('--- Stack trace: ---')
