092330635f61bf6281d8ac2ef86c9e2c7458bff6
olegoid <olegoid91@gmail.com>
2016-07-01 14:08:12 -0300

    Add fail_with_message method

--- step.rb
+++ step.rb
@@ -15,6 +15,11 @@ TOUCH_SERVER = "./Touch.Unit/Touch.Server/bin/Debug/Touch.Server.exe"
 # --- Functions
 # -----------------------
 
+def fail_with_message(message)
+  puts "\e[31m#{message}\e[0m"
+  exit(1)
+end
+
 def error_with_message(message)
   puts "\e[31m#{message}\e[0m"
 end
